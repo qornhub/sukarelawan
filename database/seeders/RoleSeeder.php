c6151367ab7d4ceca7573bdb77213a04dbc24a20
Nelson Yong <nelsonyong49@gmail.com>
2025-12-30 22:50:18 +0800

    seed

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 class RoleSeeder extends Seeder
 {
     /**
@@ -13,10 +15,11 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-           DB::table('roles')->insert([
-        ['role_id' => Str::uuid(), 'roleName' => 'volunteer'],
-        ['role_id' => Str::uuid(), 'roleName' => 'ngo'],
-        ['role_id' => Str::uuid(), 'roleName' => 'admin'],
-    ]);
+           foreach (['volunteer', 'ngo', 'admin'] as $role) {
+    Role::firstOrCreate(
+        ['roleName' => $role],
+        ['role_id' => Str::uuid()]
+    );
+}
     }
 }
