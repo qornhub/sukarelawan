ca6f45009d3c8ef76377d8025256a3512b348de4
Nelson Yong <nelsonyong49@gmail.com>
2026-01-01 22:46:56 +0800

    BLOG CATEGORY

--- app/Http/Controllers/Admin/BlogCategoryController.php
+++ app/Http/Controllers/Admin/BlogCategoryController.php
@@ -12,12 +12,12 @@ class BlogCategoryController extends Controller
     public function index()
     {
         $categories = BlogCategory::orderBy('categoryName')->get();
-        return view('admin.blogcategory.category-list', compact('categories'));
+        return view('admin.blogCategory.category-list', compact('categories'));
     }
 
     public function create()
     {
-        return view('admin.blogcategory.category-create');
+        return view('admin.blogCategory.category-create');
     }
 
     public function store(Request $request)
@@ -31,14 +31,14 @@ class BlogCategoryController extends Controller
             'categoryName'    => $request->categoryName,
         ]);
 
-        return redirect()->route('admin.blogcategory.category-list')
+        return redirect()->route('admin.blogCategory.category-list')
             ->with('success', 'Blog category created successfully.');
     }
 
     public function edit($id)
     {
         $category = BlogCategory::where('blogCategory_id', $id)->firstOrFail();
-        return view('admin.blogcategory.category-edit', compact('category'));
+        return view('admin.blogCategory.category-edit', compact('category'));
     }
 
     public function update(Request $request, $id)
@@ -52,7 +52,7 @@ class BlogCategoryController extends Controller
         $category->categoryName = $request->categoryName;
         $category->save();
 
-        return redirect()->route('admin.blogcategory.category-list')
+        return redirect()->route('admin.blogCategory.category-list')
             ->with('success', 'Blog category updated successfully.');
     }
 
@@ -61,7 +61,7 @@ class BlogCategoryController extends Controller
         $category = BlogCategory::where('blogCategory_id', $id)->firstOrFail();
         $category->delete();
 
-        return redirect()->route('admin.blogcategory.category-list')
+        return redirect()->route('admin.blogCategory.category-list')
             ->with('success', 'Blog category deleted successfully.');
     }
 }
