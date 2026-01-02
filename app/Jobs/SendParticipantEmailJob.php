7955e34888bed972b438d59c071f2f0594557737
Nelson Yong <nelsonyong49@gmail.com>
2026-01-02 14:38:23 +0800

    email2

--- app/Jobs/SendParticipantEmailJob.php
+++ app/Jobs/SendParticipantEmailJob.php
@@ -4,14 +4,15 @@ namespace App\Jobs;
 
 use App\Mail\EventParticipantMail;
 use Illuminate\Bus\Queueable;
-//use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
-class SendParticipantEmailJob 
+
+class SendParticipantEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
