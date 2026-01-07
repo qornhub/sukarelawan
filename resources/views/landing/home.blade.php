82a6323394e490bae3fa27e4112124a8e35013bf
Nelson Yong <nelsonyong49@gmail.com>
2026-01-08 00:28:26 +0800

    lamding badegs added css

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     {{-- Animate.css --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    
+    <link rel="stylesheet" href="{{ asset('css/landing_badges.css') }}">
     <style>
         :root {
             --primary-gradient: linear-gradient(135deg, #004aad 0%, #0066cc 100%);
